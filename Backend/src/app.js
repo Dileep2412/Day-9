82183672c161468d191dd49263158c7cf73eca2f
Dileep Parihar <179743478+Dileep2412@users.noreply.github.com>
2026-03-01 00:28:30 +0530

    Initial commit

---
+++ Backend/src/app.js
@@ -0,0 +1,94 @@
+/* 
+  --server ko create karna
+  -- server ko config karna
+*/
+
+const express = require ("express");
+const noteModel = require("./models/notes.model");  
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+/* 
+  
+*/
+app.use(express.static("./public"))
+
+
+
+/* 
+*-POST /api/notes
+*-create new note and save it to database
+*-req.body={title:"", description:""}
+*/
+
+app.post("/api/notes", async (req, res) => {
+     const {title, description} = req.body;
+
+     const note = await noteModel.create({
+        title,description
+     })
+
+     res.status(201).json({
+      message: "Note created successfully",
+      note  
+     });
+})
+
+/* 
+* -GET /api/notes
+* -get all notes from database and send it to client
+*/
+
+app.get("/api/notes",async (req,res)=>{
+  const notes =await noteModel.find()
+
+  res.status(200).json({
+    message: "Notes fetched successfully",
+    notes
+  })
+})
+
+/* 
+*-DELETE /api/notes/:id
+*-delete a note from database by id
+*-req.params={id:""}  
+*/
+
+app.delete("/api/notes/:id", async (req,res)=>{
+    const id = req.params.id;
+
+     await noteModel.findByIdAndDelete(id)
+
+     res.status(200).json({
+      message: "Note deleted successfully."
+     })
+})
+
+/* 
+*-PATCH /api/notes/:id
+*-update a description from database by id
+*-req.params={id:""}
+*-req.body={description:""}
+*/
+
+app.patch("/api/notes/:id",async(req,res)=>{
+    const id = req.params.id;
+    const {description} = req.body;
+
+  await  noteModel.findByIdAndUpdate(id,{description})
+
+    res.status(200).json({
+      message: "Note updated successfully."
+    })
+})
+
+app.use('*name',(req,res)=>{
+   res.sendFile(path.join(__dirname,"..","/public/index.html")) 
+})
+
+
+module.exports = app;
